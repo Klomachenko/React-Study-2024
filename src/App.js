cb5bdde210276102b3d9ba0fcbb22bb150b643b6
Klomachenko <leekyumin0901@naver.com>
2024-08-13 01:33:50 +0900

    feat: choice items Description: callback function 주의

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
 import Box from './component/Box';
 
+const choice = {
+  rock: {
+    name: 'Rock',
+    img: 'https://static.wikia.nocookie.net/sonsoftheforest_gamepedia_en/images/f/fe/RockThumb.png/revision/latest?cb=20230516015144',
+  },
+  scissors: {
+    name: 'Scissors',
+    img: 'https://assets.katogroup.eu/i/katogroup/VT8-0919-24_02_victorinox',
+  },
+  paper: {
+    name: 'Paper',
+    img: 'https://m.media-amazon.com/images/I/61OorFhm6SL._AC_UF894,1000_QL80_.jpg',
+  },
+};
+
 function App() {
+  const play = (userChoice) => {
+    console.log('선택됨', userChoice);
+  };
+
   return (
     <div>
       <div className='main'>
@@ -10,9 +29,9 @@ function App() {
       </div>
 
       <div className='main'>
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play('scissors')}>가위</button>
+        <button onClick={() => play('rock')}>바위</button>
+        <button onClick={() => play('paper')}>보</button>
       </div>
     </div>
   );
